0be1162b8c019f2de3b86253b06f884410591eff
Kristian <73885850+kriboe90@users.noreply.github.com>
2023-08-01 19:25:50 -0400

    change structure of method calls

--- RadianceMethod/test.py
+++ RadianceMethod/test.py
@@ -1,23 +1,13 @@
-import matplotlib.pyplot as plt
 from RadianceMethod import RadianceMethod
 
 test = RadianceMethod()
-
-test.image_name_string = 'DSC{:05d}.JPG'
-test.image_dir = '/Users/kristianboerger/repos/RadianceMethod/test_images'
-
-# test.image_dir = '/Volumes/N-1/22_04_27/V006/sony_1/'
-
-test.number_of_rois = 100
-test.reference_image_id = 8711
-test.first_image_id = 8711
-test.last_image_id = 8713
-test.skip_n_images = 0
-
-test.dark_roi_pixel_bounds = ((2044, 2015), (2044, 665))
-test.light_roi_pixel_bounds = ((2083, 2015), (2083, 665))
-
+test.set_experiment_name("Testexperiment")
+test.set_image_dir('/Users/kristianboerger/repos/RadianceMethod/test_images')
+test.set_image_series(8711,8713, 'DSC{:05d}.JPG')
+test.set_reference_image_id(8711)
+test.set_roi_pixel_bounds(((2044, 2015), (2044, 665)),((2083, 2015), (2083, 665)), 10, 100)
+test.set_roi_real_bounds(((0,0),(0,3)),((0.1,0),(0.1,3)))
 test.calc_roi_pixel_positions()
+test.calc_roi_real_positions()
 test.plot_reference_image_with_roi()
 test.process_image_data()
-
