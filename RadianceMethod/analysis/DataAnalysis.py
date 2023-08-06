29809c51a3bd6c09a95c9f66ac7527810f0e2715
Kristian <73885850+kriboe90@users.noreply.github.com>
2023-08-06 19:01:01 -0400

    Refactoring

--- RadianceMethod/analysis/DataAnalysis.py
+++ RadianceMethod/analysis/DataAnalysis.py
@@ -12,16 +12,16 @@ class DataAnalysis():
         self.camera_to_dark_roi_real_distances = None
         self.camera_to_light_roi_real_distances = None
         self.experiment_name = None
-        self.chanels_to_analyse = [0,1,2]
+        self.channels_to_analyse = [0, 1, 2]
 
     def set_channels_to_analyse(self, channels_to_analyse):
-        self.chanels_to_analyse = channels_to_analyse
+        self.channels_to_analyse = channels_to_analyse
 
     def load_result_data(self):
         self.results_dict = {}
 
         for cb_face in ["dark", "light"]:
-            for channel in self.chanels_to_analyse:
+            for channel in self.channels_to_analyse:
                 file_path = os.path.join(self.results_dir, f'{self.experiment_name}_{cb_face}_values_channel_{channel}.csv')
                 self.results_dict[f"{cb_face}_roi_channel_{channel}"] = pd.read_csv(file_path, skiprows=2)
 
@@ -31,7 +31,7 @@ class DataAnalysis():
         self.camera_to_light_roi_real_distances = np.loadtxt(os.path.join(self.results_dir, 'light_roi_to_camera_real_distances.csv'),delimiter=',')
 
     def calc_intensities(self):
-        for channel in self.chanels_to_analyse:
+        for channel in self.channels_to_analyse:
             dark_results_df = self.results_dict[f"dark_roi_channel_{channel}"]
             light_results_df = self.results_dict[f"light_roi_channel_{channel}"]
 
@@ -45,15 +45,12 @@ class DataAnalysis():
             file_path = os.path.join(self.results_dir, f"intensities_channel_{channel}.csv")
             intensities_df.to_csv(file_path)
 
-    def calc_extinction_coefficients(self):
+    def calc_extinction_coefficients(self): #TODO: Change form on how extinction coefficients are stored
         sigma = lambda intensity, distance : -1 * np.log(intensity) / distance
-        for channel in self.chanels_to_analyse:
+        for channel in self.channels_to_analyse:
             file_path = os.path.join(self.results_dir, f"intensities_channel_{channel}.csv")
             intensities_df = pd.read_csv(file_path, skiprows=2)
             intensities = intensities_df.iloc[:,4:].to_numpy()
             extinction_coefficients = sigma(intensities, self.camera_to_dark_roi_real_distances) #TODO: distances are calculated for dark ROIs only right now
             file_path = os.path.join(self.results_dir, f"extinction_coefficients_channel_{channel}.csv")
             np.savetxt(file_path, extinction_coefficients, delimiter=',')
-
-
-
